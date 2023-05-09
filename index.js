77d3f2cd88f854d0c2f4dc27788eb88516b75fbc
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-05-09 06:51:31 -0300

    arquivo ts convertido para js

---
+++ index.js
@@ -0,0 +1,8 @@
+function multiplicacao(numero1, numero2) {
+    return numero1 * numero2;
+}
+function saudacao(nome) {
+    return "Ol\u00E1 ".concat(nome, "!");
+}
+console.log(multiplicacao(2, 10));
+console.log(saudacao('Davis'));
